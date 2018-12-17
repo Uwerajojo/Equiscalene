dc4515c60fef307051843a402362cb64f73fc3be
Uwerajojo <josianeuwera23@gmail.com>
2018-12-17 10:48:33 +0200

    replaced function Run

--- iso.js
+++ iso.js
@@ -1,7 +1,8 @@
-function myTriangle() {
+function Run() {
     var side1=document.getElementById("fraction").value;
     alert(side1);
     var side2=document.getElementById("decimal").value;
     alert(side2);
-    
+    var side3=document.getElementById("integer").value;
+    alert(side3);
 }
